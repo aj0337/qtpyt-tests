cf728d1663c20dc3671f2125105cb0fc90d404cf
Anooja Jayaraj <Aanoojajayaraj@gmail.com>
2024-07-23 13:26:57 +0200

    Add most recent tests on AuBDA13

---
+++ AuBDA13/struct1/localized_orbitals/bda_active/get_los.py
@@ -0,0 +1,48 @@
+import numpy as np
+from ase.io import write
+from gpaw import restart
+from gpaw.lcao.pwf2 import LCAOwrap
+from qtpyt.basis import Basis
+from qtpyt.lo.tools import lowdin_rotation, rotate_matrix, subdiagonalize_atoms
+from qttools.gpaw.los import LOs
+from pathlib import Path
+
+lowdin = True
+cc_path = Path('../../dft/device/')
+gpwfile = f'{cc_path}/scatt.gpw'
+hsfile = 'hs_lolw_no_sign_flip_no_lowdin_k.npy'
+
+atoms, calc = restart(gpwfile, txt=None)
+lcao = LCAOwrap(calc)
+
+fermi = calc.get_fermi_level()
+H = lcao.get_hamiltonian()
+S = lcao.get_overlap()
+H -= fermi * S
+
+nao_a = np.array([setup.nao for setup in calc.wfs.setups])
+basis = Basis(atoms, nao_a)
+
+x = basis.atoms.positions[:, 0]
+scatt = np.where((x > 12.9) & (x < 19))[0]
+
+basis_p = basis[scatt]
+index_p = basis_p.get_indices()
+Usub, eig = subdiagonalize_atoms(basis, H, S, a=scatt)
+# for idx_lo in index_p:
+#     if Usub[idx_lo - 1, idx_lo] < 0.:  # change sign
+#         Usub[:, idx_lo] *= -1
+
+H = rotate_matrix(H, Usub)
+S = rotate_matrix(S, Usub)
+
+# if lowdin:
+#     Ulow = lowdin_rotation(H, S, index_p)
+
+#     H = rotate_matrix(H, Ulow)
+#     S = rotate_matrix(S, Ulow)
+
+#     U = Usub.dot(Ulow)
+
+np.save('idx_los.npy', index_p)
+np.save(hsfile, (H[None, ...], S[None, ...]))
---
+++ AuBDA13/struct2/dft/leads/leads.py
@@ -0,0 +1,37 @@
+import numpy as np
+from ase.io import read
+from ase.units import Hartree
+from gpaw import *
+from gpaw.lcao.tools import get_lcao_hamiltonian
+from gpaw.mpi import rank, MASTER
+
+atoms = read('leads.xyz')
+basis = 'dzp'
+
+calc = GPAW(h=0.2,
+            xc='PBE',
+            basis='szp(dzp)',
+            occupations=FermiDirac(width=0.1),
+            kpts={
+                'size': (5, 4, 2),
+                'gamma': True
+            },
+            mode='lcao',
+            txt='leads.txt',
+            mixer=Mixer(0.1, 5, weight=100.0),
+            symmetry={
+                'point_group': False,
+                'time_reversal': True
+            })
+
+atoms.set_calculator(calc)
+atoms.get_potential_energy()
+calc.write('leads.gpw', mode='all')
+
+fermi = calc.get_fermi_level()
+print(fermi, file=open('fermi_leads.txt', 'w'))
+
+H_skMM, S_kMM = get_lcao_hamiltonian(calc)
+if rank == 0:
+    H_kMM = H_skMM[0]
+    np.save('hs_leads_k.npy', (H_kMM, S_kMM))
