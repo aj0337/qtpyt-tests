ad6053470b544efcde7045676227ea47c337b1ec
Anooja Jayaraj <Aanoojajayaraj@gmail.com>
2024-09-02 12:49:56 +0200

    Create folder for Benzyl radical tests
    -Add DFT files for Benzyl radical

---
+++ AuBDA13CH2/dft/device/scatt.py
@@ -0,0 +1,25 @@
+from __future__ import print_function
+
+from ase import *
+from ase.io import read
+from gpaw import *
+
+atoms = read('scatt.xyz')
+basis = {'Au':'szp(dzp)','H':'dzp','C':'dzp','N':'dzp'}
+
+calc = GPAW(h=0.2,
+            xc='PBE',
+            basis=basis,
+            occupations=FermiDirac(width=0.2),
+            kpts=(1, 1, 1),
+            mode='lcao',
+            txt='scatt.txt',
+            mixer=Mixer(0.1, 5, weight=100.0),
+            symmetry={'point_group': False, 'time_reversal': True})
+
+atoms.set_calculator(calc)
+atoms.get_potential_energy()
+calc.write('scatt.gpw',mode='all')
+
+fermi = calc.get_fermi_level()
+print(repr(fermi), file=open('fermi.txt', 'w'))
