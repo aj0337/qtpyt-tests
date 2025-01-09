9badaadb7498bbb5a98501aa591a54719be994a9
Anooja Jayaraj <Aanoojajayaraj@gmail.com>
2025-01-09 07:03:31 +0100

    Start from a DFT calculation closer to the reference results

---
+++ AuBDA13CH2/fresh_start/get_dft_transmission.py
@@ -0,0 +1,50 @@
+from __future__ import annotations
+
+import pickle
+
+import numpy as np
+from mpi4py import MPI
+from qtpyt.block_tridiag import greenfunction
+from qtpyt.parallel import comm
+from qtpyt.parallel.egrid import GridDesc
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+
+def run(outputfile):
+    gd = GridDesc(z_ret, 1, float)
+    T = np.empty(gd.energies.size)
+    for e, energy in enumerate(gd.energies):
+        T[e] = gf.get_transmission(energy)
+
+    T = gd.gather_energies(T)
+
+    if comm.rank == 0:
+        np.save(outputfile, (z_ret, T.real))
+
+
+data_folder = "./output"
+index_active_region = np.load(f"{data_folder}/index_active_region.npy")
+self_energy = np.load(f"{data_folder}/self_energy.npy", allow_pickle=True)
+z_ret = np.load(f"{data_folder}/retarded_energies.npy")
+eta = z_ret.imag[0]
+
+with open(f"{data_folder}/hs_list_ii.pkl", "rb") as f:
+    hs_list_ii = pickle.load(f)
+with open(f"{data_folder}/hs_list_ij.pkl", "rb") as f:
+    hs_list_ij = pickle.load(f)
+
+nodes = [0, 810, 1116, 1278, 1584, 2394]
+
+# Initialize the Green's function solver with the tridiagonalized matrices and self-energies
+gf = greenfunction.GreenFunction(
+    hs_list_ii,
+    hs_list_ij,
+    [(0, self_energy[0]), (len(hs_list_ii) - 1, self_energy[1])],
+    solver="dyson",
+    eta=eta,
+)
+
+# Transmission function for DFT
+outputfile = f"{data_folder}/dft_transmission.npy"
+run(outputfile)
