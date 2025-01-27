78ebbb9f866d9d1fa98b55b15b644f1f875303a7
Anooja Jayaraj <anoojajayaraj@gmail.com>
2025-01-27 11:10:06 +0100

    Add dos data to output file

--- AuBDA13CH2/fresh_start_local/ed/ed.py
+++ AuBDA13CH2/fresh_start_local/ed/ed.py
@@ -7,7 +7,7 @@ from edpyt.espace import build_espace, screen_espace
 from edpyt.gf2_lanczos import build_gf2_lanczos
 from edpyt.shared import params
 from matplotlib import pyplot as plt
-
+from pathlib import Path
 
 class Sigma:
     def __init__(self, gf0, gf, eta=1e-5):
@@ -28,7 +28,7 @@ class Sigma:
 path = Path("../output/lowdin/")
 H_eff = np.load(path / "effective_hamiltonian.npy")
 nimp = H_eff.shape[0]
-occupancy_goal = np.load("../output/lowdin/occupancy/occupancies_gfp.npy")
+occupancy_goal = np.load("../output/lowdin/occupancies_gfp_mu_0.0.npy")
 de = 0.01
 energies = np.arange(-2, 2 + de / 2.0, de).round(7)
 eta = 1e-2
@@ -36,7 +36,8 @@ beta = 1000.0
 params["z"] = occupancy_goal
 V = np.loadtxt("../output/lowdin/U_matrix.txt")
 DC = np.diag(V.diagonal() * (occupancy_goal - 0.5))
-neig = get_active_neig(nimp, [(nimp // 2, nimp // 2)], 3)
+# neig = get_active_neig(nimp, [(nimp // 2, nimp // 2)], 3)
+neig = None
 
 espace, egs = build_espace(H_eff, np.zeros_like(H_eff), neig_sector=neig)
 screen_espace(espace, egs, beta)
@@ -49,12 +50,19 @@ screen_espace(espace, egs, beta)
 gf = build_gf2_lanczos(H_eff - DC, V, espace, beta, egs)
 DOS = -1 / np.pi * gf(energies, eta).imag.trace(axis1=0, axis2=1)
 
+output_dir = Path("../output/ed")
+output_dir.mkdir(exist_ok=True)
+
+sigma = Sigma(gf0, gf, eta=eta)
+sig = sigma.retarded(energies)
+np.save(output_dir / "sigma.npy", sig)
+
+np.save(output_dir / "energies.npy", energies)
+np.save(output_dir / "DOS0.npy", DOS0)
+np.save(output_dir / "DOS.npy", DOS)
+
 plt.plot(energies, DOS0, label="U=0")
 plt.plot(energies, DOS, label="U=matrix")
 plt.legend()
 plt.yscale("log")
-plt.show()
-
-sigma = Sigma(gf0, gf, eta=eta)
-sig = sigma.retarded(energies)
-np.save("../output/lowdin/ed/ed_sigma.npy", sig)
+plt.savefig(output_dir / "dos.png")
