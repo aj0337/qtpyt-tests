Fix spin transmission computation

pentadienyl/get_spin_dmft_transmission.py
@@ -20,14 +20,8 @@ class DataSelfEnergy(BaseDataSelfEnergy):
         return expand(S_molecule_identity, super().retarded(energy), idx_molecule)
 
 
-def load_spin_dmft_sigma(filename):
-    """Load spin-resolved DMFT self-energy and return two separate DataSelfEnergy objects"""
-    sigma = np.load(filename)
-    sigma = np.transpose(sigma, (0, 3, 1, 2))
-    return [
-        DataSelfEnergy(energies, sigma[nspin, ...]) for nspin in range(sigma.shape[0])
-    ]
-
+def load(filename):
+    return DataSelfEnergy(energies, np.load(filename))
 
 def run(outputfile):
     gd = GridDesc(energies, 1, float)
@@ -45,7 +39,6 @@ data_folder = "./output/lowdin"
 dmft_data_folder = "./output/lowdin/dmft/spin"
 index_active_region = np.load(f"{data_folder}/index_active_region.npy")
 self_energy = np.load(f"{data_folder}/self_energy.npy", allow_pickle=True)
-dmft_sigma_file = f"{dmft_data_folder}/dmft_sigma.npy"
 
 de = 0.01
 energies = np.arange(-3, 3 + de / 2.0, de).round(7)
@@ -72,15 +65,15 @@ S_molecule = hs_list_ii[imb][1]
 S_molecule_identity = np.eye(S_molecule.shape[0])
 idx_molecule = index_active_region - nodes[imb]
 
-if comm.rank == 0:
-    dmft_sigma = load_spin_dmft_sigma(dmft_sigma_file)
-else:
-    dmft_sigma = [None, None]
+for spin, spin_label in enumerate(["up", "dw"]):
+    dmft_sigma_file = f"{dmft_data_folder}/sigma_dmft_{spin_label}.npy"
+    if comm.rank == 0:
+        dmft_sigma = load(dmft_sigma_file)
+    else:
+        dmft_sigma = [None]
 
-dmft_sigma = comm.bcast(dmft_sigma, root=0)
-
-for spin, spin_label in enumerate(["up", "down"]):
-    self_energy[2] = dmft_sigma[spin]
+    dmft_sigma = comm.bcast(dmft_sigma, root=0)
+    self_energy[2] = dmft_sigma
     gf.selfenergies.append((imb, self_energy[2]))
 
     outputfile = f"{dmft_data_folder}/dmft_transmission_{spin_label}.npy"