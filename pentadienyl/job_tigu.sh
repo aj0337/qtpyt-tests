6daab7826fc244c234f61dc8b0c545dbdbe56ccd
Anooja Jayaraj <anoojajayaraj@gmail.com>
2025-03-12 08:16:07 +0000

    Add job file for tigu

---
+++ pentadienyl/job_tigu.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -l
+#SBATCH --no-requeue
+#SBATCH --job-name="defs1"
+#SBATCH --get-user-env
+#SBATCH --output=_scheduler-stdout.txt
+#SBATCH --error=_scheduler-stderr.txt
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=4-12:00:00
+#SBATCH --mem=62500
+
+export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK:-1}
+ulimit -s unlimited
+
+MINICONDA_PATH=/home/jayn/miniconda3
+
+source $MINICONDA_PATH/etc/profile.d/conda.sh
+conda activate qtpyt
