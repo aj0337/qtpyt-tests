a2aa5ddd0275e4211cf9d955a4b58e07c44db63d
Anooja Jayaraj <anoojajayaraj@gmail.com>
2025-03-21 10:51:30 +0100

    Fix centering of device to avoid extra atoms in xyz file

--- AuOPV5/structures/sort_positions_maintain_device_geometry.py
+++ AuOPV5/structures/sort_positions_maintain_device_geometry.py
@@ -6,7 +6,7 @@ The output is written to a new extended XYZ file with consistent column alignmen
 """
 
 # Load the original extended xyz file
-file_path = "relaxed/scatt_original.xyz"
+file_path = "unrelaxed/scatt_original.xyz"
 with open(file_path, "r") as f:
     data = f.readlines()
 
@@ -22,14 +22,22 @@ for line in data[2 : num_atoms + 2]:
     x, y, z = map(float, parts[1:4])
     atoms.append((element, x, y, z))
 
+# Compute the unit cell size along x-direction
+x_positions = [atom[1] for atom in atoms]
+x_min, x_max = min(x_positions), max(x_positions)
+x_unit_cell = x_max - x_min
+x_mid = x_min + x_unit_cell / 2
+
+print(f"x_mid = {x_mid}, x_unit_cell = {x_unit_cell}")
+
 
 # Define sorting keys based on position in the device
 # Assuming Au atoms define the leads and non-Au atoms define the molecule
 def sorting_key(atom):
     element, x, y, z = atom
-    if element == "Au" and x < -5:  # Left lead
+    if element == "Au" and x < x_mid:  # Left lead
         block = 0
-    elif element == "Au" and x > 5:  # Right lead
+    elif element == "Au" and x > x_mid:  # Right lead
         block = 2
     else:  # Molecule
         block = 1
@@ -40,7 +48,7 @@ def sorting_key(atom):
 sorted_atoms = sorted(atoms, key=sorting_key)
 
 # Write the sorted structure to a new extended xyz file with better alignment
-output_path_aligned = "/mnt/data/scatt.xyz"
+output_path_aligned = "unrelaxed/scatt.xyz"
 with open(output_path_aligned, "w") as f:
     f.write(f"{num_atoms}\n{comment}\n")
     for atom in sorted_atoms:
