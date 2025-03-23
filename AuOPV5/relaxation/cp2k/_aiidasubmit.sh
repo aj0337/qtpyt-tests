d83a9cd1f6033a2a0bc814910f4a82b3a6943299
Anooja Jayaraj <anoojajayaraj@gmail.com>
2025-03-23 10:25:56 +0100

    Add input and output files for cp2k geometry optimization

--- AuOPV5/relaxation/cp2k/_aiidasubmit.sh
+++ AuOPV5/relaxation/cp2k/_aiidasubmit.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -l
 #SBATCH --no-requeue
 #SBATCH --job-name="aiida-3427"
-#SBATCH --partition=debug
+#SBATCH --partition=normal
 #SBATCH --get-user-env
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=96
 #SBATCH --cpus-per-task=1
-#SBATCH --time=00:20:00
+#SBATCH --time=10:00:00
 #SBATCH --hint=nomultithread
 #SBATCH --no-requeue
 #SBATCH --account=s1276
