c4c828744123b6b9338d48c61dd370fa832df55e
Anooja Jayaraj <anoojajayaraj@gmail.com>
2025-03-25 07:35:12 +0100

    Start AuCO calculations to treat as reference data for qtpyt

---
+++ AuCO/block_tridiagonal_gf/get_dft_dos.py
@@ -0,0 +1,60 @@
+from __future__ import annotations
+
+import pickle
+
+import numpy as np
+from mpi4py import MPI
+from qtpyt.block_tridiag import greenfunction
+from qtpyt.parallel import comm
+from qtpyt.parallel.egrid import GridDesc
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+
+
+def run(outputfile_pdos, outputfile_dos, run_dos=False):
+    gd = GridDesc(energies, 1, float)
+    pdos = np.empty(gd.energies.size)
+    for e, energy in enumerate(gd.energies):
+        pdos[e] = gf.get_pdos(energy)
+
+    if run_dos:
+        dos = np.empty(gd.energies.size)
+        for e, energy in enumerate(gd.energies):
+            dos[e] = gf.get_dos(energy)
+        dos = gd.gather_energies(dos)
+        if comm.rank == 0:
+            np.save(outputfile_dos, (energies, dos.real))
+
+    pdos = gd.gather_energies(pdos)
+
+    if comm.rank == 0:
+        np.save(outputfile_pdos, (energies, pdos.real))
+
+
+data_folder = "./output/lowdin"
+self_energy = np.load(f"{data_folder}/self_energy.npy", allow_pickle=True)
+
+de = 0.1
+energies = np.arange(-1, 1 + de, de)
+eta = 1e-3
+
+with open(f"{data_folder}/hs_list_ii.pkl", "rb") as f:
+    hs_list_ii = pickle.load(f)
+with open(f"{data_folder}/hs_list_ij.pkl", "rb") as f:
+    hs_list_ij = pickle.load(f)
+
+nodes = [0, 5616, 10192, 15296]
+
+# Initialize the Green's function solver with the tridiagonalized matrices and self-energies
+gf = greenfunction.GreenFunction(
+    hs_list_ii,
+    hs_list_ij,
+    [(0, self_energy[0]), (len(hs_list_ii) - 1, self_energy[1])],
+    solver="spectral",
+    eta=eta,
+)
+
+outputfile_pdos = f"{data_folder}/dft_pdos.npy"
+outputfile_dos = f"{data_folder}/dft_dos.npy"
+run(outputfile_pdos, outputfile_dos, run_dos=True)
--- AuCO/dft/device/dump.py
+++ AuCO/dft/device/dump.py
@@ -1,14 +1,15 @@
 import numpy as np
 from ase.io import read
-from ase.units import Hartree
 from gpaw import *
 from gpaw.lcao.tools import get_lcao_hamiltonian
-from gpaw.mpi import rank, MASTER
+from gpaw.mpi import rank
 
-atoms = read('scatt.xyz')
+atoms = read("scatt.xyz")
+output_folder = "output"
 
-calc = GPAW(restart='scatt.gpw',txt=None)
-atoms.set_calculator(calc)
+
+calc = GPAW(restart=f"{output_folder}/scatt.gpw", txt=None)
+atoms.calc = calc
 calc.wfs.set_positions(calc.spos_ac)
 
 fermi = calc.get_fermi_level()
@@ -17,4 +18,4 @@ H_skMM, S_kMM = get_lcao_hamiltonian(calc)
 if rank == 0:
     H_kMM = H_skMM[0]
     H_kMM -= fermi * S_kMM
-    np.save('hs_cc_k.npy', (H_kMM, S_kMM))
+    np.save(f"{output_folder}/hs_cc_k.npy", (H_kMM, S_kMM))
