4e330bbd0d2159ed7aad028c0802b4c22338c6cb
Anooja Jayaraj <anoojajayaraj@gmail.com>
2025-05-19 15:57:44 +0200

    Add files for DFT calculation of unstrained CNT

---
+++ strained_CNT/dft/device/scatt.py
@@ -0,0 +1,28 @@
+from __future__ import print_function
+
+from ase import *
+from ase.io import read
+from gpaw import *
+
+atoms = read("initial.xyz")
+basis = {"C": "szp(dzp)"}
+
+kbt = 0.01
+calc = GPAW(
+    h=0.2,
+    xc="PBE",
+    basis=basis,
+    occupations=FermiDirac(width=kbt),
+    kpts=(1, 1, 1),
+    mode="lcao",
+    txt="initial.txt",
+    mixer=Mixer(0.02, 5, weight=100.0),
+    symmetry={"point_group": False, "time_reversal": True},
+)
+
+atoms.set_calculator(calc)
+atoms.get_potential_energy()
+calc.write("initial.gpw", mode="all")
+
+fermi = calc.get_fermi_level()
+print(repr(fermi), file=open("fermi.txt", "w"))
