a6485702146add08bd28ca16cb4c77d671a7d39a
Anooja Jayaraj <anoojajayaraj@gmail.com>
2025-12-17 11:16:38 +0100

    Add scripts for gw+edmft tests

---
+++ gw_edmft/AuBDA14/dft/leads/leads.py
@@ -0,0 +1,33 @@
+import numpy as np
+from ase.io import read
+from gpaw import *
+from gpaw.lcao.tools import get_lcao_hamiltonian
+from gpaw.mpi import rank
+
+atoms = read("leads.xyz")
+basis = "szp(dzp)"
+
+kbt = 1e-2
+calc = GPAW(
+    h=0.2,
+    xc="LDA",
+    basis=basis,
+    occupations=FermiDirac(width=kbt),
+    kpts=(5, 5, 3),
+    mode="lcao",
+    txt="leads.txt",
+    mixer=Mixer(0.02, 5, weight=100.0),
+    symmetry={"point_group": False, "time_reversal": True},
+)
+
+atoms.calc = calc
+atoms.get_potential_energy()
+calc.write("leads.gpw")
+
+fermi = calc.get_fermi_level()
+print(fermi, file=open("fermi.txt", "w"))
+
+H_skMM, S_kMM = get_lcao_hamiltonian(calc)
+if rank == 0:
+    H_kMM = H_skMM[0]
+    np.save("hs_pl_k.npy", (H_kMM, S_kMM))
---
+++ gw_edmft/AuBDA14/job_alps.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -l
+#SBATCH --job-name=aubda13ch2-los
+#SBATCH --time=00:30:00
+#SBATCH --partition=debug
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --constraint=gpu
+#SBATCH --hint=nomultithread
+#SBATCH --no-requeue
+#SBATCH --account=lp83
+#SBATCH --uenv=prgenv-gnu/24.7:v3
+
+export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK:-1}
+ulimit -s unlimited
+
+MINICONDA_PATH=/users/ajayaraj/miniconda3
+
+source $MINICONDA_PATH/etc/profile.d/conda.sh
+conda activate qtpyt
+
+# mpirun -n 1 python get_los_prerequisites.py
+mpirun -n 1 python get_gf_prerequisites.py
+mpirun -n 1 python get_dft_states.py
+# mpirun -n 200 python get_dft_transmission.py
