08922da1785bdd8db4082de23df9c40886928ecc
Anooja Jayaraj <anoojajayaraj@gmail.com>
2026-01-16 09:54:29 +0100

    Start tests on converting LO to AO basis

---
+++ vertex_correction/pentadienyl/ao_basis/get_dft_transmission.py
@@ -0,0 +1,125 @@
+import os
+from pathlib import Path
+import matplotlib.pyplot as plt
+
+import numpy as np
+from ase.io import read
+from qtpyt.base.greenfunction import GreenFunction
+from qtpyt.base.selfenergy import DataSelfEnergy as BaseDataSelfEnergy
+from qtpyt.basis import Basis
+from qtpyt.parallel import comm
+from qtpyt.parallel.egrid import GridDesc
+from qtpyt.projector import expand
+from qtpyt.surface.principallayer import PrincipalSelfEnergy
+from qtpyt.surface.tools import prepare_leads_matrices
+from qtpyt.tools import expand_coupling, remove_pbc, rotate_couplings
+
+rank = comm.Get_rank()
+
+
+class DataSelfEnergy(BaseDataSelfEnergy):
+    """Wrapper"""
+
+    def retarded(self, energy):
+        return expand(S_device, super().retarded(energy), idx_molecule)
+
+
+def load(filename):
+    return DataSelfEnergy(energies, np.load(filename))
+
+
+def run(outputfile):
+    gd = GridDesc(energies, 1, float)
+    T_total = np.empty(gd.energies.size)
+    T_elastic = np.empty(gd.energies.size)
+    T_inelastic = np.empty(gd.energies.size)
+    for e, energy in enumerate(gd.energies):
+        T_total[e] = gf.get_transmission(energy)
+
+    T_total = gd.gather_energies(T_total)
+
+    if comm.rank == 0:
+        np.save(outputfile, (energies, T_total.real, T_elastic.real, T_inelastic.real))
+
+        plt.figure()
+        plt.plot(energies, T_total.real, label="Total")
+        plt.yscale("log")
+        plt.xlim(-3.0, 3.0)
+        plt.ylim(1e-5, 1)
+        plt.xlabel("Energy (eV)")
+        plt.ylabel("Transmission")
+        plt.tight_layout()
+        plt.savefig(f"{output_folder}/ET_lo_dft.png", dpi=300)
+        plt.close()
+
+
+pl_path = Path("./dft/leads/")
+cc_path = Path("./dft/device/")
+output_folder = "./output/lowdin/dft"
+os.makedirs(output_folder, exist_ok=True)
+
+data_folder = "./output/lowdin"
+index_active_region = np.load(f"{data_folder}/index_active_region.npy")
+self_energy = np.load(f"{data_folder}/self_energy.npy", allow_pickle=True)
+
+H_leads_lcao, S_leads_lcao = np.load(pl_path / "hs_pl_k.npy")
+H_subdiagonalized, S_subdiagonalized = map(
+    lambda m: m.astype(complex),
+    np.load("./output/lowdin/hs_los_lowdin.npy"),
+)
+
+basis_dict = {"Au": 9, "H": 5, "C": 13, "N": 13}
+
+leads_atom = read(pl_path / "leads.xyz")
+leads_basis = Basis.from_dictionary(leads_atom, basis_dict)
+
+device_atoms = read(cc_path / "scatt.xyz")
+device_basis = Basis.from_dictionary(device_atoms, basis_dict)
+
+de = 0.01
+energies = np.arange(-3.0, 3.0 + de / 2.0, de).round(7)
+eta = 1e-3
+
+Nr = (1, 5, 3)
+unit_cell_rep_in_leads = (5, 5, 3)
+
+kpts_t, h_leads_kii, s_leads_kii, h_leads_kij, s_leads_kij = prepare_leads_matrices(
+    H_leads_lcao,
+    S_leads_lcao,
+    unit_cell_rep_in_leads,
+    align=(0, H_subdiagonalized[0, 0, 0]),
+)
+
+remove_pbc(device_basis, H_subdiagonalized)
+remove_pbc(device_basis, S_subdiagonalized)
+
+# Initialize self-energy list for left and right leads
+self_energy = [None, None, None]
+self_energy[0] = PrincipalSelfEnergy(
+    kpts_t, (h_leads_kii, s_leads_kii), (h_leads_kij, s_leads_kij), Nr=Nr
+)
+self_energy[1] = PrincipalSelfEnergy(
+    kpts_t, (h_leads_kii, s_leads_kii), (h_leads_kij, s_leads_kij), Nr=Nr, id="right"
+)
+
+# Rotate the couplings for the leads based on the specified basis and repetition Nr
+rotate_couplings(leads_basis, self_energy[0], Nr)
+rotate_couplings(leads_basis, self_energy[1], Nr)
+
+# expand to dimension of scattering
+expand_coupling(self_energy[0], len(H_subdiagonalized[0]))
+expand_coupling(self_energy[1], len(H_subdiagonalized[0]), id="right")
+
+gf = GreenFunction(
+    H_subdiagonalized[0],
+    S_subdiagonalized[0],
+    selfenergies=[(slice(None), self_energy[0]), (slice(None), self_energy[1])],
+    eta=eta,
+)
+
+# Transmission function calculation
+S_device = S_subdiagonalized[0]
+idx_molecule = index_active_region
+
+outputfile = f"{output_folder}/ET.npy"
+run(outputfile)
---
+++ vertex_correction/pentadienyl/ao_basis/job_alps_dft.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -l
+#SBATCH --job-name=aubda13ch2-los
+#SBATCH --time=00:10:00
+#SBATCH --partition=normal
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=48
+#SBATCH --cpus-per-task=1
+#SBATCH --constraint=gpu
+#SBATCH --hint=nomultithread
+#SBATCH --no-requeue
+#SBATCH --account=lp86
+#SBATCH --uenv=prgenv-gnu/24.7:v3
+
+export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK:-1}
+ulimit -s unlimited
+
+MINICONDA_PATH=/users/ajayaraj/miniconda3
+
+source $MINICONDA_PATH/etc/profile.d/conda.sh
+conda activate qtpyt
+
+
+mpirun -n 48 python get_dft_transmission.py
