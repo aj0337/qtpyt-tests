b4b7e406efd83340ae1be4b2b37270c05ce667c7
Anooja Jayaraj <anoojajayaraj@gmail.com>
2026-01-25 18:31:25 +0100

    Add PPP vs CRPA tests

---
+++ strongly_correlated_open_shell/pentadienyl/production_run/referee_response/get_ed_self_energy.py
@@ -0,0 +1,62 @@
+import os
+
+import numpy as np
+from edpyt.shared import params
+from edpyt.espace import build_espace, screen_espace
+from edpyt.gf2_lanczos import build_gf2_lanczos
+
+
+class Sigma:
+    def __init__(self, gf0, gf, H_eff, eta=1e-5):
+        self.gf0 = gf0
+        self.gf = gf
+        self.eta = eta
+        self.H_eff = H_eff
+
+    def retarded(self, energy):
+        energies = np.atleast_1d(energy)
+        g0 = self.gf0(energies, self.eta)
+        g = self.gf(energies, self.eta)
+        sigma = np.empty((energies.size, self.gf.n, self.gf.n), complex)
+        for e, energy in enumerate(energies):
+            sigma[e] = np.linalg.inv(g0[..., e]) - np.linalg.inv(g[..., e])
+        return sigma
+
+
+# === Load inputs ===
+input_folder = "../output/lowdin"
+output_folder = "../output/lowdin/ed/referee_response"
+
+os.makedirs(output_folder, exist_ok=True)
+
+H_eff = np.load(f"{input_folder}/effective_hamiltonian.npy")
+occupancy_goal = np.load(f"{input_folder}/occupancies.npy")
+V = np.loadtxt(f"{input_folder}/U_matrix_ppp_2.0.txt")
+
+# === Parameters ===
+nimp = H_eff.shape[0]
+eta = 1e-2
+beta = 1000
+
+DC = np.load(f"{output_folder}/ed_dcc_diag_Uppp_2.0.npy")
+DC = np.diag(DC)
+neig = np.ones((nimp + 1) * (nimp + 1), int) * 6
+
+params["z"] = occupancy_goal
+
+espace0, egs0 = build_espace(H_eff, np.zeros_like(H_eff), neig_sector=neig)
+screen_espace(espace0, egs0, beta)
+gf0 = build_gf2_lanczos(H_eff, np.zeros_like(H_eff), espace0, beta, egs0)
+
+espace, egs = build_espace(H_eff - DC, V, neig_sector=neig)
+screen_espace(espace, egs, beta)
+gf = build_gf2_lanczos(H_eff - DC, V, espace, beta, egs)
+
+sigma = Sigma(gf0, gf, H_eff, eta=eta)
+
+# === Calculate self-energy ===
+de = 0.01
+energies = np.arange(-3, 3 + de / 2.0, de).round(7)
+z_ret = energies + 1.0j * eta
+sigma_ret = sigma.retarded(z_ret)
+np.save(f"{output_folder}/ed_sigma_ppp_2.0.npy", sigma_ret)
---
+++ strongly_correlated_open_shell/pentadienyl/production_run/referee_response/get_ed_transmission.py
@@ -0,0 +1,97 @@
+from __future__ import annotations
+
+import pickle
+
+import numpy as np
+from mpi4py import MPI
+from qtpyt.base.selfenergy import DataSelfEnergy as BaseDataSelfEnergy
+from qtpyt.block_tridiag import greenfunction
+from qtpyt.parallel import comm
+from qtpyt.parallel.egrid import GridDesc
+import matplotlib.pyplot as plt
+from qtpyt.projector import expand
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+
+
+class DataSelfEnergy(BaseDataSelfEnergy):
+    """Wrapper"""
+
+    def retarded(self, energy):
+        return expand(S_molecule_identity, super().retarded(energy), idx_molecule)
+
+
+def load(filename):
+    return DataSelfEnergy(energies, np.load(filename))
+
+
+def run(outputfile):
+    gd = GridDesc(energies, 1, float)
+    T = np.empty(gd.energies.size)
+    for e, energy in enumerate(gd.energies):
+        T[e] = gf.get_transmission(energy, ferretti=False)
+
+    T = gd.gather_energies(T)
+
+    if comm.rank == 0:
+        np.save(outputfile, (energies, T.real))
+        plt.figure()
+        plt.plot(energies, T)
+        plt.yscale("log")
+        plt.xlim(-3.0, 3.0)
+        plt.ylim(1e-5, 1)
+        plt.xlabel("Energy (eV)")
+        plt.ylabel("Transmission")
+        plt.tight_layout()
+        plt.savefig(f"{ed_data_folder}/ET_Uppp_2.0.png", dpi=300)
+        plt.close()
+
+
+data_folder = "../output/lowdin"
+ed_data_folder = "../output/lowdin/ed/referee_response"
+index_active_region = np.load(f"{data_folder}/index_active_region.npy")
+self_energy = np.load(f"{data_folder}/self_energy.npy", allow_pickle=True)
+ed_self_energy_file = f"{ed_data_folder}/ed_sigma_ppp_2.0.npy"
+
+de = 0.01
+energies = np.arange(-3, 3 + de / 2.0, de).round(7)
+eta = 1e-2
+
+with open(f"{data_folder}/hs_list_ii.pkl", "rb") as f:
+    hs_list_ii = pickle.load(f)
+with open(f"{data_folder}/hs_list_ij.pkl", "rb") as f:
+    hs_list_ij = pickle.load(f)
+
+nodes = np.load(f"{data_folder}/nodes.npy")
+
+# Initialize the Green's function solver with the tridiagonalized matrices and self-energies
+gf = greenfunction.GreenFunction(
+    hs_list_ii,
+    hs_list_ij,
+    [(0, self_energy[0]), (len(hs_list_ii) - 1, self_energy[1])],
+    solver="dyson",
+    eta=eta,
+)
+
+# Add the DMFT self-energy for transmission
+if comm.rank == 0:
+    ed_sigma = load(ed_self_energy_file)
+else:
+    ed_sigma = None
+
+# Transmission function calculation
+imb = 2  # index of molecule block from the nodes list
+S_molecule = hs_list_ii[imb][1]  # overlap of molecule
+S_molecule_identity = np.eye(S_molecule.shape[0])
+idx_molecule = (
+    index_active_region - nodes[imb]
+)  # indices of active region w.r.t molecule
+
+ed_sigma = comm.bcast(ed_sigma, root=0)
+self_energy[2] = ed_sigma
+gf.selfenergies.append((imb, self_energy[2]))
+
+outputfile = f"{ed_data_folder}/ET_Uppp_2.0.npy"
+run(outputfile)
+gf.selfenergies.pop()
