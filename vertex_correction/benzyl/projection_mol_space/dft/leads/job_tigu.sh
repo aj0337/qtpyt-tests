14880a41dd8c7638b8818ded826ac1ba2fe61022
Anooja Jayaraj <anoojajayaraj@gmail.com>
2026-02-03 17:13:34 +0100

    Start vertex correction tests on benzyl

---
+++ vertex_correction/benzyl/projection_mol_space/dft/leads/job_tigu.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -l
+#SBATCH --no-requeue
+#SBATCH --job-name="defs1"
+#SBATCH --get-user-env
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=4
+#SBATCH --cpus-per-task=1
+#SBATCH --time=4-12:00:00
+#SBATCH --mem=2500
+
+export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK:-1}
+export MPICH_GPU_SUPPORT_ENABLED=0
+export GPAW_SETUP_PATH=${HOME}/gpaw-setups-24.11.0
+ulimit -s unlimited
+
+source "/home/jayn/miniconda3/envs/gpaw/bin/activate"
+
+mpirun -n 4 gpaw python leads.py
